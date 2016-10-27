aa9dd9ed1ff3732b619f137e7340454f144f4797
Pablo Baltodano Madrigal <Pablo Baltodano Madrigal>
2016-10-26 20:53:15 -0600

    Creación interfaz login

---
+++ login.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>M&M Soluciones</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="estilos/estilo_login.css">
+  <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+  <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+</head>
+<body>
+  <header>
+    <div id="logo">
+      <h1><img src="logo.png"></h1>
+    </div>
+    <div id="header-wrapper">
+      <div id="menu">
+      </div>
+    </div>
+  </header>
+  <br><br>
+
+  <div class="container">
+    <section id="content">
+      <form action="">
+        <h1>Iniciar Sesión</h1>
+        <div>
+          <input type="text" placeholder="Nombre de usuario" required="" id="username" />
+        </div>
+        <div>
+          <input type="password" placeholder="Contraseña" required="" id="password" />
+        </div>
+        <div>
+          <input type="submit" value="Iniciar Sesión" />
+          <a href="#">Recordar contraseña</a>
+        </div>
+      </form>
+    </section>
+  </div>
+
+  <footer id="footer">
+    <div align="center">
+      <label>Síguenos en:</label>
+      <ul class="soc">
+        <li><a class="soc-twitter" href="#"></a></li>
+        <li><a class="soc-facebook" href="#"></a></li>
+        <li><a class="soc-google" href="#"></a></li>
+        <!--<li><i class="fa fa-cc-mastercard" aria-hidden="true" style="font-size:48px;color:black"></i></li>-->
+      </ul>
+       &copy; Instituto Tecnológico de Costa Rica 2016
+    </div>
+  </footer>
+ 
+</body>
+</html>

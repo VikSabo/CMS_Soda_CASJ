9f8a90a242e318b0007a84ee6ebeaee9dc9cb463
mcisnerosaguilar <marlonca15@hotmail.com>
2016-11-03 19:35:21 -0600

    están las 2 ya

--- crearBebida.php
+++ crearBebida.php
@@ -5,23 +5,23 @@
 
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		//Post variables
-		$nombrePlato = $_POST['nombreplato'];
-		$descripcionPlato = $_POST['descripcionplato'];
-		$precioPlato = $_POST['precioplato'];
+		$nombreBebida = $_POST['nombrebebida'];
+		$descripcionBebida = $_POST['descripcionbebida'];
+		$precioBebida = $_POST['preciobebida'];
 		$miArchivo = $_POST['myFile'];
-		$direccionTotal = 'platos_img/'.$miArchivo;
+		$direccionTotal = 'bebidas_img/'.$miArchivo;
 		
 		//if($nombrePlato or $descripcionPlato or $precioPlato === null){
 		//echo "Exiten campos en blanco, refrescar y agregar correctamente";
 		//}
 		
 		
-	$sql = "INSERT INTO `plato`(`nombre_plato`, `descripcion`, `precio`, `imagen`) VALUES ('$nombrePlato','$descripcionPlato','$precioPlato', '$direccionTotal')";
+	$sql = "INSERT INTO `bebida`(`nombre_bebida`, `descripcion`, `precio`, `imagen`) VALUES ('$nombreBebida','$descripcionBebida','$precioBebida', '$direccionTotal')";
 
   
   	// Insert the data if the query its ok
 		if ($connection->query($sql) === TRUE) {
-		    echo "<div class='success'>Plato agregado correctamente</div>";
+		    echo "<div class='success'>Bebida agregada correctamente</div>";
 		} else {
 		    echo "Error: " . $sql . "<br>" . $connection->error;
 		}
@@ -34,8 +34,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Crear Plato</title>
-  <link rel="stylesheet" type="text/css" href="estilos/estilo_crearPlato.css">
+  <title>Crear Bebida</title>
+  <link rel="stylesheet" type="text/css" href="estilos/estilo_crearBebida.css">
   <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
   <link rel="stylesheet" type="text/css" href="estilos/estilo_index.css"> 
   <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
@@ -45,20 +45,21 @@
 
   <br><br>
 
-  <h1 align = "center">Formulario para Agregar una Comida Nueva</h1>
+  <h1 align = "center">Formulario para Agregar una Bebida Nueva</h1>
+  <br>
   <div align = "center">
   <section>
   <!-- meter lo del form -->
    <div class="formulario" align="center">
     <form action="" method="post">
-      <label for="lname">Ingrese el nuevo Plato:</label>
-      <input type="text" id="nombreplato" name="nombreplato"><br>
+      <label for="lname">Ingrese el nombre:</label>
+      <input type="text" id="nombrebebida" name="nombrebebida"><br>
 	  <br>
-	  <label for="lname">Descripción del Platillo:</label>
-      <input type="text" id="descripcionplato" name="descripcionplato"><br>
+	  <label for="lname">Descripción de la bebida:</label>
+      <input type="text" id="descripcionbebida" name="descripcionbebida"><br>
 	  <br>
-	  <label for="lname">Ingrese el precio del Platillo:</label>
-      <input type="number" id="precioplato" name="precioplato"><br>
+	  <label for="lname">Ingrese el precio de la Bebida:</label>
+      <input type="number" id="preciobebida" name="preciobebida"><br>
 	  <br>
 	  <input type="file" id="myFile" name="myFile">
 	  <script>
@@ -69,12 +70,13 @@
 	  </script>
 	  <br>
 	  <br>
-	  <input type="submit" value="Agregar Plato"/><br>
+	  <input type="submit" value="Agregar Bebida"/><br>
     </form>
   </div>
  </section>
  </div>
  
+
 
 </body>
 </html>

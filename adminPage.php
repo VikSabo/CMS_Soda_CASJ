6c525347b99ae691a9640cc0db93cedc9984ce02
Pablo Baltodano Madrigal <Pablo Baltodano Madrigal>
2016-11-03 22:12:38 -0600

    Data show - tab Cotizaciones (adminPage)

--- adminPage.php
+++ adminPage.php
@@ -1,5 +1,27 @@
 <?php
    include_once('session.php');
+   include_once ('connection_db.php');
+
+	$connection = db_connect();
+
+	$sql = "SELECT * FROM cotizar";
+	$result = mysqli_query($connection, $sql);
+
+	if (mysqli_num_rows($result) > 0) {
+		echo '<table cellpadding="0" cellspacing="0" class="db-table">';
+		echo '<tr><th>ID</th><th>NOMBRE</th><th>APELLIDO</th><th>TELÉFONO</th><th>EMAIL</th><th>PROVINCIA</th><th>DIRECCIÓN</th><th>FECHA (EVENTO)</th><th>NÚMERO DE PERSONAS</th><th>SERVICIO</th><th>INFORMACIÓN ADICIONAL</th></tr>';
+	    // output data of each row
+	    while($row = mysqli_fetch_assoc($result)) {
+	    	echo '<tr>';
+			foreach($row as $key=>$value) {
+				echo '<td>',$value,'</td>';
+			}
+			echo '</tr>';
+	    }
+	} else {
+	    echo "0 results";
+	}
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -7,6 +29,7 @@
 	<title>Welcome</title>
 	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 	<link rel="stylesheet" type="text/css" href="estilos/estilo_general.css">
+	<link rel="stylesheet" type="text/css" href="estilos/estilo_adminPage.css">
 </head>
 <body>
 	<div id="header">
@@ -25,13 +48,12 @@
 	        <li><a href="#"><span>Crear Nueva Bebida</span></a></li>
 	      </ul>
 	    </div>
-	
 	  </div>
 	</div>
 
 
 	<div id="container">
-		
+		<h1>Cotizaciones</h1>
 	</div>
 </body>
 </html>
